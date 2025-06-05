d5364d162b3cd41af291dc2125f9f15e2a7dd058
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2025-06-05 10:40:49 -0300

    Initial commit

---
+++ src/main/java/com/antonybresolin/ai_generator_controller/AiGeneratorControllerApplication.java
@@ -0,0 +1,13 @@
+package com.antonybresolin.ai_generator_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeneratorControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGeneratorControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonybresolin/ai_generator_controller/AiGeneratorControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonybresolin.ai_generator_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeneratorControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
